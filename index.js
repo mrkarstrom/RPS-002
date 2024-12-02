1d913c2ecd13031fe02644b7cf9cfce40f78a315
Magnus <mrkarstrom81@gmail.com>
2024-12-02 10:12:41 +0100

    feat (index.js): Set up the express server to run at port 3000.

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+// import gameRoutes from './src/routes/gameRoutes.js';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the Rock-Paper-Scissor-API.' });
+});
+
+// app.use('/api/games', gameRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is now open on http://localhost:${port}`);
+});
